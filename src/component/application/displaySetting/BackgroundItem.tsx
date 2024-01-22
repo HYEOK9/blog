28227127785a7e0a77313276e3b8afe2564746f5
HYEOK9 <leejaehyuck9@gmail.com>
2024-01-22 17:45:24 +0900

    __css

--- src/component/application/displaySetting/BackgroundItem.tsx
+++ src/component/application/displaySetting/BackgroundItem.tsx
@@ -10,14 +10,14 @@ export default function BackgroundItem({ image }: BackgroundItemProps) {
   const [load, setLoad] = useState(false);
   const { setImage } = backgroundStore();
 
-  const skeleton = load ? "" : "w-52 h-52 animate-pulse bg-gray-700";
+  const skeleton = load ? "" : "animate-pulse bg-gray-700";
 
   return (
     <div key={image.name} className="text-center mb-5">
       <Image
         width={208}
         height={208}
-        className={`${skeleton} h-52 rounded-lg mb-1 border-4 border-transparent hover:border-blue-main dark:hover:border-gray-300`}
+        className={`w-52 h-52 rounded-lg mb-1 border-4 border-transparent hover:border-blue-main dark:hover:border-gray-300 ${skeleton}`}
         src={image.src}
         alt={image.name}
         onClick={() => setImage(image)}
