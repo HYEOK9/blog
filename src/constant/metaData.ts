2b2a88717fba39ad617626aed157a9ac6c65dec0
HYEOK9 <leejaehyuck9@gmail.com>
2024-04-08 17:38:55 +0900

    [FEAT] og:image

--- src/constant/metaData.ts
+++ src/constant/metaData.ts
@@ -1,4 +1,4 @@
-export const META_TITLE = "HYEOK9 블로그" as const;
+export const META_TITLE = "HYEOK9" as const;
 export const META_DESCRIPTION = "이재혁 프론트엔드 포트폴리오" as const;
 export const META_OG_IMAGE =
-  "https://github.com/HYEOK9/blog/assets/87190744/60687b9c-f94f-43ad-a1b4-fdeaf262530d" as const;
+  `${process.env.AWS_CLOUDFRONT_URL}/ogImage.png` as const;
